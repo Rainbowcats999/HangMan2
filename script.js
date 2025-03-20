f41b59019f47cb59ec7e7b52dbec1c3c31a18c3b
Rainbowcats999 <526erizov@frhsd.com>
2025-03-20 08:52:29 -0400

    Added time variables

---
+++ script.js
@@ -0,0 +1,25 @@
+const wordList = [
+  "cats",
+  "kittens",
+  "dog",
+  "school",
+  "rain",
+  "charm",
+  "gold",
+  "silver",
+  "bronze",
+  "gay",
+  "lesbian",
+  "bisexual",
+  "transgender",
+  "intersex",
+  "queer",
+  "asexual",
+];
+
+//setting time variables
+let selectedWord = "";
+let displayWord = "";
+let wrongGuesses = 0;
+let guessedLetters = [];
+const maxMistakes = 6;
